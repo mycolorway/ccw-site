b6257d96021932b7f38fa28b45eb0eb6653ecc87
farthinker <farthinker@gmail.com>
2011-05-09 12:48:16 +0800

    调整了banner动画的速度

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -37,8 +37,8 @@ function transition( index ) {
     }
     
     slides.eq( currentIndex )
-        .fadeOut( 1000 );
-    newSlide.fadeIn( 1000, function() {
+        .fadeOut( "slow" );
+    newSlide.fadeIn( "slow", function() {
         $( this ).addClass( "cur" )
             .siblings( "li" )
             .removeClass( "cur" );
