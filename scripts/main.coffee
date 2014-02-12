bc5b764cea379a9d7432845270875e1a2df10b4e
Ruochen <ruochen@mycolorway.com>
2014-02-12 22:05:34 +0800

    bug fix

--- scripts/main.coffee
+++ scripts/main.coffee
@@ -1,25 +1,23 @@
 (($) ->
 
   shadow = $("#shadow-circle")
-  $(document).on "mouseover", "#page-team .earth .member", (e) ->
-    shadow.css({
-      opacity: 0.2,
+  $(document).on "mouseenter", "#page-team .earth .member", (e) ->
+    shadow.css
+      pop: '',
+      opacity: 0,
       '-moz-transform': 'scale(1)',
       '-webkit-transform': 'scale(1)',
       'transform': 'scale(1)',
       top: $(this).position().top + $(this).height()/2 - 80,
       left: $(this).position().left + $(this).width()/2 - 80
-    })
-    $(this).next('.member-info').fadeIn(500)
 
-  .on "mouseout", "#page-team .earth .member", (e) ->
-    shadow.css({
-      opacity: 0.8,
-      '-moz-transform': 'scale(0.4)',
-      '-webkit-transform': 'scale(0.4)',
-      'transform': 'scale(0.4)'
-    })
-    $(this).next('.member-info').fadeOut(500)
+    $('.member-info').stop(true, true)
+    $(this).next('.member-info').delay(200).fadeIn(500)
+
+  .on "mouseleave", "#page-team .earth .member", (e) ->
+    shadow.attr('style', '')
+    $('.member-info').stop(true, true)
+    $(this).next('.member-info').fadeOut(300)
 
 
 ) jQuery
--- scripts/main.js
+++ scripts/main.js
@@ -2,24 +2,22 @@
   (function($) {
     var shadow;
     shadow = $("#shadow-circle");
-    return $(document).on("mouseover", "#page-team .earth .member", function(e) {
+    return $(document).on("mouseenter", "#page-team .earth .member", function(e) {
       shadow.css({
-        opacity: 0.2,
+        pop: '',
+        opacity: 0,
         '-moz-transform': 'scale(1)',
         '-webkit-transform': 'scale(1)',
         'transform': 'scale(1)',
         top: $(this).position().top + $(this).height() / 2 - 80,
         left: $(this).position().left + $(this).width() / 2 - 80
       });
-      return $(this).next('.member-info').fadeIn(500);
-    }).on("mouseout", "#page-team .earth .member", function(e) {
-      shadow.css({
-        opacity: 0.8,
-        '-moz-transform': 'scale(0.4)',
-        '-webkit-transform': 'scale(0.4)',
-        'transform': 'scale(0.4)'
-      });
-      return $(this).next('.member-info').fadeOut(500);
+      $('.member-info').stop(true, true);
+      return $(this).next('.member-info').delay(200).fadeIn(500);
+    }).on("mouseleave", "#page-team .earth .member", function(e) {
+      shadow.attr('style', '');
+      $('.member-info').stop(true, true);
+      return $(this).next('.member-info').fadeOut(300);
     });
   })(jQuery);
 
