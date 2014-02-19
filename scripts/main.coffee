61a764d2792d0739a50ba8063fb44f730f616146
Ruochen <ruochen@mycolorway.com>
2014-02-19 18:07:26 +0800

    取消CDN、更改team路径、修改绝对路径

--- scripts/main.coffee
+++ scripts/main.coffee
@@ -1,26 +1,21 @@
-(($) ->
+template = (id, data) ->
+  target = $.trim($("#" + id).html())
+  if data
+    $.each data, (key, value) ->
+      re = new RegExp("\\{\\{ " + key + " \\}\\}", "g")
+      target = target.replace(re, value)
 
-  template = (id, data) ->
-    target = $.trim($("#" + id).html())
-    if data
-      $.each data, (key, value) ->
-        re = new RegExp("\\{\\{ " + key + " \\}\\}", "g")
-        target = target.replace(re, value)
+  return $ target
 
-    return $ target
+$.getJSON "../info.json", (data) ->
+  $.each data.members, (i, member) ->
+    template('tpl-member', member)
+      .appendTo($('#members'))
 
-  $.getJSON "/info.json", (data) ->
-    $.each data.members, (i, member) ->
-      template('tpl-member', member)
-        .appendTo($('#members'))
-
-  $(".footer .weixin").on "click", (e) ->
-    e.preventDefault()
-    if $(document).width() < 768
-      $(".weixin-info").css({ left: $(this).offset().left - 32 })
-        .fadeToggle()
-    else
-      $(".weixin-info").fadeToggle()
-
-
-) jQuery
+$(".footer .weixin").on "click", (e) ->
+  e.preventDefault()
+  if $(document).width() < 768
+    $(".weixin-info").css({ left: $(this).offset().left - 32 })
+      .fadeToggle()
+  else
+    $(".weixin-info").fadeToggle()
--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,34 @@
 (function() {
-  (function($) {
-    var template;
-    template = function(id, data) {
-      var target;
-      target = $.trim($("#" + id).html());
-      if (data) {
-        $.each(data, function(key, value) {
-          var re;
-          re = new RegExp("\\{\\{ " + key + " \\}\\}", "g");
-          return target = target.replace(re, value);
-        });
-      }
-      return $(target);
-    };
-    $.getJSON("/info.json", function(data) {
-      return $.each(data.members, function(i, member) {
-        return template('tpl-member', member).appendTo($('#members'));
+  var template;
+
+  template = function(id, data) {
+    var target;
+    target = $.trim($("#" + id).html());
+    if (data) {
+      $.each(data, function(key, value) {
+        var re;
+        re = new RegExp("\\{\\{ " + key + " \\}\\}", "g");
+        return target = target.replace(re, value);
       });
+    }
+    return $(target);
+  };
+
+  $.getJSON("../info.json", function(data) {
+    return $.each(data.members, function(i, member) {
+      return template('tpl-member', member).appendTo($('#members'));
     });
-    return $(".footer .weixin").on("click", function(e) {
-      e.preventDefault();
-      if ($(document).width() < 768) {
-        return $(".weixin-info").css({
-          left: $(this).offset().left - 32
-        }).fadeToggle();
-      } else {
-        return $(".weixin-info").fadeToggle();
-      }
-    });
-  })(jQuery);
+  });
+
+  $(".footer .weixin").on("click", function(e) {
+    e.preventDefault();
+    if ($(document).width() < 768) {
+      return $(".weixin-info").css({
+        left: $(this).offset().left - 32
+      }).fadeToggle();
+    } else {
+      return $(".weixin-info").fadeToggle();
+    }
+  });
 
 }).call(this);
