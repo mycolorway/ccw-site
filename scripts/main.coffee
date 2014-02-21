5ffb17872ec4267a6be95c5332d5b18a8a631978
Ruochen <ruochen@mycolorway.com>
2014-02-21 12:20:18 +0800

    fix style

--- scripts/main.coffee
+++ scripts/main.coffee
@@ -6,17 +6,3 @@ $.getJSON "./info.json", (data) ->
       .next('.title').html(m.title)    # 这里用 html() 因为有的 title 里写了标签
       .next('.location').text(m.location)
       .parent().appendTo($('#members'))
-
-weixinInfo = $(".weixin-info")
-$(".footer .weixin").on "click", (e) ->
-  e.preventDefault()
-  if $(document).width() < 768
-    weixinInfo.css({ left: $(this).offset().left - 32 })
-      .fadeToggle()
-  else
-    weixinInfo.fadeToggle()
-
-$(document).on "click", (e) ->
-  if weixinInfo.is(':visible') && !$(e.target).hasClass('weixin')
-    weixinInfo.fadeOut()
-
--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,8 @@
 (function() {
-  var weixinInfo;
-
   $.getJSON("./info.json", function(data) {
     return $.each(data.members, function(i, m) {
       return $($('#tpl-member').html()).addClass(m.nickname).find('.name').text(m.name).next('.title').html(m.title).next('.location').text(m.location).parent().appendTo($('#members'));
     });
-  });
-
-  weixinInfo = $(".weixin-info");
-
-  $(".footer .weixin").on("click", function(e) {
-    e.preventDefault();
-    if ($(document).width() < 768) {
-      return weixinInfo.css({
-        left: $(this).offset().left - 32
-      }).fadeToggle();
-    } else {
-      return weixinInfo.fadeToggle();
-    }
-  });
-
-  $(document).on("click", function(e) {
-    if (weixinInfo.is(':visible') && !$(e.target).hasClass('weixin')) {
-      return weixinInfo.fadeOut();
-    }
   });
 
 }).call(this);
